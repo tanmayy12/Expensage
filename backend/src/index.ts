1af06fcb668cf18d305a5e18d60c400a83486108
Tanmay Jagtap <tanmayjagtap12@outlook.com>
2025-07-06 21:08:03 +0530

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ import subscriptionsRoutes from "./routes/subscriptions";
 import groupsRoutes from "./routes/groups";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:8080", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(passport.initialize());
 
